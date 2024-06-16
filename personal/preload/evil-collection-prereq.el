18d858a0a8776bf5d11276f538f9c7b5712ee0d0
Senkowo <94131088+senkowo@users.noreply.github.com>
2024-06-16 05:31:19 -0400

    touch up

--- personal/preload/evil-collection-prereq.el
+++ personal/preload/evil-collection-prereq.el
@@ -1,9 +1,10 @@
 ;; Evil-collection adds evil binds to a lot of modes not supported by default.
 ;; For it to work, `evil-want-keybinding' must be nil when evil loads.
+;; A lot of the variables here need to be set before evil loads.
 ;; To add evil support for the minibuffer, it must be loaded before helm and
 ;; other minibuffer-related packages load.
 
-(setq evil-want-C-u-scroll t
-      evil-respect-visual-line-mode t)
-(setq evil-want-keybinding nil)
-(setq evil-collection-setup-minibuffer t)
+(setq evil-want-keybinding nil ; needed
+      evil-want-C-u-scroll t ; more vim-like
+      evil-respect-visual-line-mode t ; nice
+      evil-collection-setup-minibuffer t) ; minibuffer
