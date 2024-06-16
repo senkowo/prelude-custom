3f4cab7c49db3cab831b564eec1e5506dba31135
Senkowo <94131088+senkowo@users.noreply.github.com>
2024-06-16 17:50:55 -0400

    doom modeline and icons

--- personal/config-pretty.el
+++ personal/config-pretty.el
@@ -18,12 +18,36 @@
 (my/native-transparency-set 90)
 
 ;; prettier modeline
-(use-package mood-line
+;; (use-package mood-line
+;;   :ensure t
+;;   :config
+;;   (mood-line-mode 1)
+;;   (setq mood-line-glyph-alist mood-line-glyphs-fira-code)
+;;   (setq mood-line-format mood-line-format-default-extended))
+
+;; (use-package all-the-icons-nerd-fonts
+;;   :ensure t)
+
+(use-package all-the-icons
   :ensure t
   :config
-  (mood-line-mode 1)
-  (setq mood-line-glyph-alist mood-line-glyphs-fira-code)
-  (setq mood-line-format mood-line-format-default-extended))
+  (if (eq (string-to-number
+           (shell-command-to-string "fc-list | grep -c 'all-the-icons.ttf'"))
+          '0)
+      (all-the-icons-install-fonts)))
+
+(use-package all-the-icons-nerd-fonts
+  :ensure t
+  :config
+  (if (eq (string-to-number
+           (shell-command-to-string "fc-list | grep -c 'NFM.ttf'"))
+          '0)
+      (nerd-icons-install-fonts)))
+
+;; even prettier modeline
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
 
 ;; dashboard
 (use-package dashboard
